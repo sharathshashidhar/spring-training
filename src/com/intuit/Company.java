43ff4ad47d75a8a2f59b05e7bc894cbc249fc3e9
Sharath Shashidhar <sharath_shashidhar@intuit.com>
2017-10-24 11:27:37 +0530

    Constructor args

--- src/com/intuit/Company.java
+++ src/com/intuit/Company.java
@@ -1,10 +1,22 @@
 package com.intuit;
 
+import java.util.List;
+
 /**
  * Created by sshashidhar on 10/24/17.
  */
 public class Company {
-    public Company(){
+    private String name;
+    private String location;
+    private List<String> founders;
+
+    public Company(String name, String location){
+        this.name = name;
+        this.location = location;
         System.out.println("Company instantiated");
+    }
+
+    public void setFounders(List<String> founders) {
+        this.founders = founders;
     }
 }
